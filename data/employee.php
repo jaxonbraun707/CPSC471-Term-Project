1440411cb6d1ff3e32fe226b92be2b6eb5b2dea8
jaxonbraun707 <jaxon707@shaw.ca>
2022-11-23 16:19:15 -0700

    Made changes to how employees are added

--- data/employee.php
+++ data/employee.php
@@ -9,3 +9,12 @@ function get_employees_by_job($db, $job) {
 
 	return $query;
 }
+
+function add_employee($db, $SSN, $First_Name, $Last_Name, $DOB, $Phone_No, $Email, $Address_Line_1, $Address_Line_2, $City, $Prov_State, $Country, $Postal_Zip, $Job_Type) {
+	$q = "
+		INSERT INTO Employee
+		VALUES (:SSN, :First_Name, :Last_Name, :DOB, :Phone_No, :Email, :Address_Line_1, :Address_Line_2, :City, :Prov_State, :Country, :Postal_Zip, :Job_Type)
+	";
+	$query = $db->prepare($q);
+	$query->execute([':SSN' => $SSN, ':First_Name' => $First_Name, ':Last_Name' => $Last_Name, ':DOB' => $DOB, ':Phone_No' => $Phone_No, ':Email' => $Email, ':Address_Line_1' => $Address_Line_1, ':Address_Line_2' => $Address_Line_2, ':City' => $City, ':Prov_State' => $Prov_State, ':Country' => $Country, ':Postal_Zip' => $Postal_Zip, ':Job_Type' => $Job_Type]);
+}
