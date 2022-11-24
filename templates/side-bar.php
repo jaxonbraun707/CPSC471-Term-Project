d8922a209997e5bddff4c81340d804a06c273c81
Gareth Jenkins <65422175+jenkoman@users.noreply.github.com>
2022-11-23 21:24:05 -0700

    First commit

--- templates/side-bar.php
+++ templates/side-bar.php
@@ -1,8 +1,10 @@
 <nav class="w-64 h-screen fixed pt-20 border">
 	<ul>
 		<li class="py-2 px-4">
-			<a href="#">Clients</a>
+			<a href=clients.php#">Clients</a>
 		</li>
+		<li class="py-2 px-4"><a href="proposals.php#">Proposals</li>
+		<li class="py-2 px-4"><a href="contracts.php#">Contracts</li>
 		<li class="py-2 px-4">Projects</li>
 		<li class="py-2 px-4">Designs</li>
 	</ul>
