a57e8db27f732f8b7a7a2c79f00d2949b1607504
Rupert Amodia <rupertraphael@live.com>
2022-11-25 16:13:37 -0700

    Fix paths for login and designs.

---
+++ env.example.php
@@ -0,0 +1,8 @@
+<?php
+// set the base URL
+// e.g. for XAMPP projects: localhost/project
+define("BASE_URL", "localhost/CPSC-471-Term-Project");
+
+// set the base path
+// e.g. C:\xampp\htdocs\project
+define("BASE_PATH", $_SERVER['DOCUMENT_ROOT']);
