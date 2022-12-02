48d727d9fbabb54ba1425467e9e5cafe464caedc
Rupert Amodia <rupertraphael.amodia@ucalgary.ca>
2022-12-02 15:17:14 -0700

    Implement fix for sidebar links and logout (#11)

--- templates/side-bar.php
+++ templates/side-bar.php
@@ -1,16 +1,16 @@
 <nav class="w-64 h-screen fixed pt-20 border">
 	<ul>
 		<li class="py-2 px-4">
-			<a href="../clients/index.php#">Clients</a>
+			<a href="<?=BASE_URL ?>/clients/">Clients</a>
 		</li>
-		<li class="py-2 px-4"><a href="../proposals/index.php#">Proposals</li>
-		<li class="py-2 px-4"><a href="../contracts/index.php#">Contracts</li>
+		<li class="py-2 px-4"><a href="<?=BASE_URL ?>/proposals/">Proposals</a></li>
+		<li class="py-2 px-4"><a href="<?=BASE_URL ?>/contracts/">Contracts</a></li>
 		<li class="py-2 px-4">Projects</li>
 		<li class="py-2 px-4">
-			<a href="../designs" class="hover:text-blue-500">Designs</a>
+			<a href="<?=BASE_URL ?>/designs/" class="hover:text-blue-500">Designs</a>
 		</li>
 		<li class="py-2 px-4">
-			<a href="../employees" class="hover:text-blue-500">Employees</a>
+			<a href="<?=BASE_URL ?>/employees/" class="hover:text-blue-500">Employees</a>
 		</li>
 	</ul>
 </nav>
