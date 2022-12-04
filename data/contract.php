4f605b453cfaa95282a8cbaa75fe74893bb2ff6f
Rupert Amodia <rupertraphael@live.com>
2022-12-03 19:09:01 -0700

    Remove implementation of get_contracts.

--- data/contract.php
+++ data/contract.php
@@ -1,12 +1,5 @@
 <?php
 function get_contracts($db) {
-	$q = "
-		SELECT  
-			    C.*, P.Title
-		FROM Contract AS C, Proposal AS P
-        WHERE 
-            C.Proposal_No = P.Proposal_No
-		";
-	return $db->query($q);
+	return false;
 }
 ?>
