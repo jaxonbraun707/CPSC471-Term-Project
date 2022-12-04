05ce63f4929e0bbb0da961db6aed8a795078c8a3
Rupert Amodia <rupertraphael@live.com>
2022-12-04 15:35:08 -0700

    Some fixes to designs.

--- designs/edit.php
+++ designs/edit.php
@@ -154,7 +154,7 @@ include('../templates/top-bar.php');
 									<?=$drawing['Drawing_No']?>
 								</td>
 								<td class="text-right py-2">
-									<form method="POST" action="/<?=BASE_URL?>/designs/delete_drawing.php">
+									<form method="POST" action="delete_drawing.php">
 										<input type="hidden" name="design_no" value="<?=$design['Design_No']?>">
 										<input type="hidden" name="drawing" value="<?=$drawing['Drawing_No']?>">
 										<button type="submit" class="hover:text-red-500">Remove</button>
@@ -166,7 +166,7 @@ include('../templates/top-bar.php');
 							?>
 						</tbody>
 					</table>
-					<form method="POST" action="/<?=BASE_URL?>/designs/add_drawing.php">
+					<form method="POST" action="add_drawing.php">
 						<input type="hidden" name="design_no" value="<?=$design['Design_No']?>">
 						<label>Add a drawing:</label>
 						<input type="number" name="drawing" class="py-2 border rounded pl-2 w-32">
